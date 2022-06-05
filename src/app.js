54722bb8246b6593d43793e4882bc78908c9c004
Dereje <getutadessedereje@icloud.com>
2022-06-05 19:58:33 +0200

    Update: pass data php

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
-const fs = require('fs');
+
 const img = document.querySelector('img');
 const like = document.querySelector('.fa-heart');
 let keyUnsplah = "DNOXP05XZ1QlAN2NxzLND4N6Gt2J5iSkIKnEtiJ_hAw";
+
+// get day formated
+
+// get day fomated with name day and month name and year name
+function getDay(){
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let dayName = date.toLocaleString('fr-FR', {weekday: 'long'});
+    let monthName = date.toLocaleString('fr-FR', {month: 'long'});
+    return ` le ${dayName} ${day} ${monthName} ${year}`;
+}
 
 async function getData(key,mood){
     let url =`https://api.unsplash.com/search/photos?query=${mood}&client_id=${key}`;
@@ -9,11 +22,27 @@ async function getData(key,mood){
     const data = await response.json();
     let imgUrl = data.results[Math.floor(Math.random() * data.results.length)].urls.full;
     img.setAttribute('src', imgUrl);
-
-    // button like
+    let id = 0;
     like.addEventListener('click', () => {
-
+        let data = new FormData();
+        data.append(`url${id++}`, imgUrl);
+        data.append(`mood`, mood);
+        data.append('le :', getDay());
+        addDataBase(data);
     })
 }
 
-getData(keyUnsplah,'city');
+getData(keyUnsplah,'storm');
+
+function addDataBase(data){
+    fetch('./script_php/like.php', {
+        method: 'POST',
+        body: data,
+    })
+    .then((res)=>{
+        return res.json();
+    })
+    .then((data)=>{
+        console.log(data);
+    })
+}
