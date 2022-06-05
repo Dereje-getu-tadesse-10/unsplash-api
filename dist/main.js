3976d3ff63c830c903d328085c4d0fb0ed2b96f4
Dereje <getutadessedereje@icloud.com>
2022-06-05 15:43:47 +0200

    Update: img dans le main

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \********************/
 /***/ (() => {
 
-eval("\nlet keyUnsplah = \"6BWyhvXSEyO3egDZ8E8UXW01I4-pElsxeaTMjx8ZN4Q\";\nlet url =`https://api.unsplash.com/photos/?client_id=${keyUnsplah}`;\n\n\n//# sourceURL=webpack://api-unsplash/./src/app.js?");
+eval("const img = document.querySelector('img');\n\nlet keyUnsplah = \"6BWyhvXSEyO3egDZ8E8UXW01I4-pElsxeaTMjx8ZN4Q\";\nlet url =`https://api.unsplash.com/photos/?client_id=${keyUnsplah}`;\n\n// fetch await data from unsplash\n\n// fecth data from unsplash\nfetch(url)\n.then((response) =>{\n    return response.json();\n}\n)\n.then((data) => {\n    let src = data[0].urls.regular;\n    img.setAttribute('src', src);\n    let alt = data[0].alt_description;\n    img.setAttribute('alt', alt);\n\n\n\n\n})\n\n\n//# sourceURL=webpack://api-unsplash/./src/app.js?");
 
 /***/ })
 
--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+const img = document.querySelector('img');
 
-
+let keyUnsplah = "6BWyhvXSEyO3egDZ8E8UXW01I4-pElsxeaTMjx8ZN4Q";
 let url =`https://api.unsplash.com/photos/?client_id=${keyUnsplah}`;
+
+// fetch await data from unsplash
+
+// fecth data from unsplash
+fetch(url)
+.then((response) =>{
+    return response.json();
+}
+)
+.then((data) => {
+    let src = data[0].urls.regular;
+    img.setAttribute('src', src);
+    let alt = data[0].alt_description;
+    img.setAttribute('alt', alt);
+
+})
